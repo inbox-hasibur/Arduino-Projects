ed1310c185756f75ec85a827ca83187e0f330504
Hasibur Rahman <113811769+inbox-hasibur@users.noreply.github.com>
2024-11-23 23:00:34 +0600

    Update Optimized LFR.ino

--- Optimized LFR.ino
+++ Optimized LFR.ino
@@ -7,6 +7,7 @@
 int s[6];
 int base [6] = {1, 2, 4, 8, 16, 32);
 int sensor;
+
 void setup() {
 pinMode(1mf, OUTPUT);
 pinMode(1mb, OUTPUT);
@@ -14,9 +15,32 @@ pinMode(rmf, OUTPUT);
 pinMode(rmb, OUTPUT);
 // motor(250, 250); //run all the motor forward at full speed
 }
+
+void motor (int a, int b) {
+  if (a > 0) {
+    digitalWrite(lmf, 1);
+    digitalWrite(lmb, 0);
+  }
+  else {
+    a = (a);
+    digitalWrite(lmf, 0);
+    digitalWrite(lmb, 1);
+  }
+  if (b > 0) {
+    digitalWrite(rmf, 1);
+    digitalWrite(rmb, 0);
+  }
+  else { 
+    b = b;
+    digitalWrite(rmf, 0);
+    digitalWrite(rmb, 1);
+  }
+  analogWrite(lms, a);
+  analogWrite(rms, b);
+}
 void loop() {
-reading();
-if (sensor == 0b001100) motor (250, 250);
-else if (sensor = 0b001000) motor (200, 250);
-else if (sensor == 0b100000) motor(0, 200);
+  reading();
+  if (sensor == 0b001100) motor (250, 250);
+  else if (sensor = 0b001000) motor (200, 250);
+  else if (sensor == 0b100000) motor(0, 200);
 }
